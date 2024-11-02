6a5ca1bee853a08f858ea37ef81a1c54c8e6555b
Maikon dos Santos Leite <104660287+MaikonLeite@users.noreply.github.com>
2024-11-01 23:19:10 -0300

    Add files via upload

---
+++ project/js/layouts/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Zodiac Horoscope</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
+    <link rel="stylesheet" type="text/css" href="/project/assets/css/style.css">
+
+
+</head>
+<body>
+    <!-- Conteúdo da página vai aqui -->
+</body>
+</html>
